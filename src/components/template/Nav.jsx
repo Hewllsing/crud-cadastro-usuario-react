449a3b22f3db1803c726c0cf2546674851107ea1
Leonardo Souza <leonardo_23s@outlook.com>
2025-02-12 15:15:05 +0000

    criando crud e adicionando lógica para armazenar nos dados

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -1,12 +1,15 @@
 import './Nav.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props => 
     <aside className="menu-area">
-        <a href='#/'>
-            <i className='fa fa-home'></i> Inicio
-        </a>
-        <a href='#/users'>
-            <i className='fa fa-users'></i> Usuario
-        </a>
+        <nav className='menu'>
+            <Link to='/'>
+                <i className='fa fa-home'></i> Inicio
+            </Link>
+            <Link to='/users'>
+                <i className='fa fa-users'></i> Usuario
+            </Link>
+        </nav>
     </aside>
